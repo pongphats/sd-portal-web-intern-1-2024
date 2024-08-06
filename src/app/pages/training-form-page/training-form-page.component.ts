e197947b8946febfdb0cce58431bda240e153731
Pongphat <pongphats@pccth.com>
2024-08-06 15:53:42 +0700

    feat : add new training forms page and binding forms

--- src/app/pages/training-form-page/training-form-page.component.ts
+++ src/app/pages/training-form-page/training-form-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { trainingForm } from 'src/app/interface/form';
 
 @Component({
   selector: 'app-training-form-page',
@@ -7,7 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingFormPageComponent implements OnInit {
 
-  constructor() { }
+  trainingForm!: FormGroup<trainingForm>
+  constructor(private  fb:FormBuilder) {
+    this.trainingForm = this.fb.group({
+      company: ['' , Validators.required],
+      deptCode: ['', Validators.required],
+      deptId: [''],
+      addMissionDate: ['', Validators.required],
+      formsType: ['', Validators.required],
+      courseName: ['', Validators.required],
+      courseObjective: [''],
+      courseDuration: [''],
+      courseDescription: [''],
+      courseProject: [''],
+      coursePrice: [''],
+      courseTeacher: [''],
+      courseLocation: [''],
+      budgetType: ['', Validators.required],
+      budgetDescription: [''],
+      employeeId: ['', Validators.required],
+      employeeName: ['', Validators.required],
+      employeePosition: ['', Validators.required],
+      approverName: [''],
+      managerName: [''],
+      vicePresName: [''],
+      vicePresName2: [''],
+      presidentName: [''],
+    });
+   }
 
   ngOnInit(): void {
   }
