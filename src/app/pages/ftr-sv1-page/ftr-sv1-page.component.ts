954b136647311e104f8ade0eb99c230ea5abec71
songwut90123 <songwut90123@gmail.com>
2024-08-07 08:43:40 +0700

    ubdate : api save

--- src/app/pages/ftr-sv1-page/ftr-sv1-page.component.ts
+++ src/app/pages/ftr-sv1-page/ftr-sv1-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { budgetForm } from 'src/app/interface/form';
+import { saveBudgetByYearRequest } from 'src/app/interface/request';
+import { ApiService } from 'src/app/services/api.service';
 import { CommonService } from 'src/app/services/common.service';
-import { SwalService } from 'src/app/services/swal.service';
 
 @Component({
   selector: 'app-ftr-sv1-page',
@@ -16,7 +17,7 @@ export class FtrSv1PageComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private commonService: CommonService,
-    private swalService: SwalService
+    private apiService: ApiService
   ) {}
 
   async ngOnInit() {
@@ -28,11 +29,45 @@ export class FtrSv1PageComponent implements OnInit {
       budgetCer: ['', Validators.required],
       budgetTotal: [{ value: '', disabled: true }],
     });
+    this.budgetForm.get('budgetTrain')?.valueChanges.subscribe(() => {
+      this.updateTotal();
+    });
+
+    this.budgetForm.get('budgetCer')?.valueChanges.subscribe(() => {
+      this.updateTotal();
+    });
   }
+
+  // API  ของ depts
   protected genDeptsByCompanyName() {
     const res = this.budgetForm.controls.company.value || '';
     this.commonService.getOnlyDeptCodeByCompany(res).subscribe((depts) => {
       this.depts = depts;
     });
+  }
+
+  // ผลรวมของ  budgetTrain +  budgetCer =  budgetTotal
+  protected updateTotal() {
+    const trainValue = Number(this.budgetForm.get('budgetTrain')?.value || 0);
+    const cerValue = Number(this.budgetForm.get('budgetCer')?.value || 0);
+    const total = trainValue + cerValue;
+    this.budgetForm.get('budgetTotal')?.setValue(total.toString());
+  }
+
+  async onSave() {
+    const company_id = this.commonService.getCompanyIdByName(
+      this.budgetForm.value.company || ''
+    );
+    console.log(company_id);
+    const req: saveBudgetByYearRequest = {
+      year: this.budgetForm.value.budgetYear || '',
+      deptCode: this.budgetForm.value.dept || '',
+      company_id: Number(company_id),
+      budgetTraining: this.budgetForm.value.budgetTrain || '',
+      budgetCer: this.budgetForm.value.budgetCer || '',
+    };
+
+    const res = await this.apiService.saveBudgetByYear(req).toPromise();
+    console.log(res);
   }
 }
