148598123ae02230deaa8499c87639f4458ecf68
orangutan11 <orangutan11112000@gmail.com>
2024-08-07 15:24:34 +0700

    update [manage-course-page] : add table

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -6,7 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { NgxMatTimepickerModule } from 'ngx-mat-timepicker';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
-
+import {MatTableModule} from '@angular/material/table';
 @NgModule({
   exports: [
     MatDatepickerModule,
@@ -15,6 +15,7 @@ import { MatSelectModule } from '@angular/material/select';
     NgxMatTimepickerModule,
     MatRadioModule,
     MatSelectModule,
+    MatTableModule
   ],
   providers: [DatePipe],
 })
