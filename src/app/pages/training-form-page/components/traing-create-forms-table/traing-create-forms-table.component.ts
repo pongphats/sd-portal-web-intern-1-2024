4167949b2d4272e0543d5bab2482483ff4f830ff
Pongphat <pongphats@pccth.com>
2024-08-08 13:49:54 +0700

    feat : prepare service training list table

--- src/app/pages/training-form-page/components/traing-create-forms-table/traing-create-forms-table.component.ts
+++ src/app/pages/training-form-page/components/traing-create-forms-table/traing-create-forms-table.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-traing-create-forms-table',
   templateUrl: './traing-create-forms-table.component.html',
-  styleUrls: ['./traing-create-forms-table.component.scss']
+  styleUrls: ['./traing-create-forms-table.component.scss'],
 })
 export class TraingCreateFormsTableComponent implements OnInit {
+  trainingList: any[] = [];
 
-  constructor() { }
+  constructor(private commonService: CommonService) {}
 
   ngOnInit(): void {
+    this.initTrainingList();
   }
 
+  initTrainingList(): void {
+    this.commonService.getTrainingList().subscribe((value: any[]) => {
+      if (value) {
+        this.trainingList = value;
+      }
+    });
+  }
 }
