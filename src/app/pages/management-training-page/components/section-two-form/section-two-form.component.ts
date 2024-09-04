983d8ad4ff75864a8d68b7e8ec537c9c3a210c95
orangutan11 <orangutan11112000@gmail.com>
2024-09-04 15:04:49 +0700

    update [management-training-page > section-two-form] : สร้าง formGroup

--- src/app/pages/management-training-page/components/section-two-form/section-two-form.component.ts
+++ src/app/pages/management-training-page/components/section-two-form/section-two-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { TrainingService } from 'src/app/services/training.service';
 
 @Component({
   selector: 'app-section-two-form',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionTwoFormComponent implements OnInit {
 
-  constructor() { }
+  approverForm!: FormGroup;
+  evaluatorForm!: FormGroup;
+  ressonForm!: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private trainingService: TrainingService,
+    private fb: FormBuilder,
+  ) {
+    this.approverForm = this.fb.group({
+      evaluatorName: [''],
+      evaluatorPosition: [''],
+      evaluatorDept: [''],
+      evaluatorSector: [''],
+    })
+
+    this.evaluatorForm = this.fb.group({
+      result1 : [''],
+      result2 : [''],
+      result3 : [''],
+      result4 : [''],
+      result5 : [''],
+      result6 : [''],
+      result7 : [''],
+    })
+
+    this.ressonForm = this.fb.group({
+      comment: ['555'],
+      conclusion: ['pass'],
+      cause1: ['oh'],
+      cause2: ['mg'],
+    })
   }
 
+  async ngOnInit() {
+    await this.initSectionTwo();
+  }
+
+  async initSectionTwo() {
+    console.log('pure : initSectionTwo')
+    try {
+      const data = this.trainingService.trainingEditData;
+
+      const approveData = data.training.approve1
+      // console.log("approver : ", approveData)
+      this.approverForm.controls['evaluatorName'].setValue(
+        approveData.firstname + ' ' + approveData.lastname
+      );
+      this.approverForm.controls['evaluatorPosition'].setValue(
+        approveData.position.positionName
+      );
+      this.approverForm.controls['evaluatorSector'].setValue(
+        approveData.sector.sectorName
+      );
+      this.approverForm.controls['evaluatorDept'].setValue(
+        approveData.department.deptName
+      );
+
+
+    } catch (error) {
+      console.error(error);
+    } 
+  }
 }
